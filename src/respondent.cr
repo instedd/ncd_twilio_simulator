80f585d4bba0a8229ab7451f61980e1e6642d1a0
aeatencio <aeatencio@gmail.com>
2020-11-10 10:33:30 -0300

    Add the missing hashtag

--- src/respondent.cr
+++ src/respondent.cr
@@ -1,7 +1,7 @@
 class Respondent
   def Respondent.reply_message(ao_message : AOMessage) : ReplyCommand | Nil
     case ao_message.message
-    when "hangup"
+    when "#hangup"
       HangUp.new(ao_message)
     when /#numeric|#oneof/
       PressDigits.new(ao_message, 1)
