0d652537b071bdeda81684820a3046c9c419abd3
Andrés Atencio <aatencio@manas.tech>
2020-11-11 18:47:07 -0300

    Use named tuples instead of hashes
    Co-authored-by: Brian J. Cardiff <bcardiff@manas.com.ar>

--- src/twiliosim.cr
+++ src/twiliosim.cr
@@ -41,7 +41,7 @@ class Twiliosim::Server
   private def handle_incoming_phone_numbers_request(context)
     context.response.status_code = 200
     context.response.content_type = "application/json"
-    response = {"sid" => UUID.random().to_s()}
+    response = {sid: UUID.random().to_s()}
     response.to_json(context.response)
   end
 
