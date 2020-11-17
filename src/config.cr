ea89c50780c2727903b4e35a08de5bd9571b2d2d
aeatencio <aeatencio@gmail.com>
2020-11-17 12:26:33 -0300

    Default config values
    So the env variables aren't required anymore
    
    Dor #16

--- src/config.cr
+++ src/config.cr
@@ -15,33 +15,42 @@ struct Twiliosim::Config
   property sticky_respondents : Bool
 
   def initialize
-    @no_reply_percent = Config.float_env("NO_REPLY_PERCENT")
-    @unresponsive_timeout_seconds = Config.int_env("UNRESPONSIVE_TIMEOUT_SECONDS")
-    @delay_reply_min_seconds = Config.int_env("DELAY_REPLY_MIN_SECONDS")
-    @delay_reply_max_seconds = Config.int_env("DELAY_REPLY_MAX_SECONDS")
-    @incorrect_reply_percent = Config.float_env("INCORRECT_REPLY_PERCENT")
-    @max_incorrect_reply_value = Config.int_env("MAX_INCORRECT_REPLY_VALUE")
-    @sticky_respondents = Config.string_env("STICKY_RESPONDENTS") == "true"
+    @no_reply_percent = Config.float_env("NO_REPLY_PERCENT") || 0.0
+    @unresponsive_timeout_seconds = Config.int_env("UNRESPONSIVE_TIMEOUT_SECONDS") || 5
+    @delay_reply_min_seconds = Config.int_env("DELAY_REPLY_MIN_SECONDS") || 1
+    @delay_reply_max_seconds = Config.int_env("DELAY_REPLY_MAX_SECONDS") || 5
+    @incorrect_reply_percent = Config.float_env("INCORRECT_REPLY_PERCENT") || 0.0
+    @max_incorrect_reply_value = Config.int_env("MAX_INCORRECT_REPLY_VALUE") || 99
+    @sticky_respondents = sticky_respondents?
   end
 
   def self.load
     Twiliosim::Config.new
   end
 
-  protected def self.string_env(var_name)
-    unless (value = ENV[var_name]) && value != "***"
-      raise "Missing #{var_name}"
+  protected def self.string_env(var_name) : String | Nil
+    ENV[var_name] if ENV.has_key?(var_name)
+  end
+
+  protected def self.int_env(var_name) : Int32 | Nil
+    if ENV.has_key?(var_name)
+      value = string_env(var_name)
+      return unless value
+      value.to_i rescue raise "#{var_name} must be an integer"
     end
-    value
   end
 
-  protected def self.int_env(var_name)
-    value = string_env(var_name)
-    value.to_i rescue raise "#{var_name} must be an integer"
+  protected def self.float_env(var_name) Float64 | Nil
+    if ENV.has_key?(var_name)
+      value = string_env(var_name)
+      return unless value
+      value.to_f rescue raise "#{var_name} must be a float"
+    end
   end
 
-  protected def self.float_env(var_name)
-    value = string_env(var_name)
-    value.to_f rescue raise "#{var_name} must be a float"
+  private def sticky_respondents?
+    value = Config.string_env("STICKY_RESPONDENTS")
+    return true unless value
+    return value == "true"
   end
 end
