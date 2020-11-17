9175dcb55ce5f85f0df8386eb1f250c0b36f28da
aeatencio <aeatencio@gmail.com>
2020-11-17 11:38:47 -0300

    Improve expressiveness
    For #16

--- src/models/simulator.cr
+++ src/models/simulator.cr
@@ -1,32 +1,21 @@
-require "./numeric_command"
-require "./one_of_command"
+require "./simulator_command"
 
 module Twiliosim::Simulator
   def self.reply_message(ao_message : AOMessage, config : Twiliosim::Config) : ReplyCommand | Nil
-    case ao_message.message
-    when "#hangup"
-      HangUp.new(ao_message)
-    when /#numeric/
-      numeric_command = NumericCommand.parse(ao_message.message)
-      if numeric_command
-        reply = if incorrect_reply?(config)
-          numeric_command.valid_sample
-        else
-          numeric_command.invalid_sample(config)
-        end
-        PressDigits.new(ao_message, reply)
-      end
-    when /#oneof/
-      one_of_command = OneOfCommand.parse(ao_message.message)
-      if one_of_command
-        reply = if incorrect_reply?(config)
-          one_of_command.valid_sample
-        else
-          one_of_command.invalid_sample(config)
-        end
-        PressDigits.new(ao_message, reply)
-      end
+    message = ao_message.message
+    return HangUp.new(ao_message) if (message == "#hangup")
+
+    command = SimulatorCommand.parse(message)
+    return unless command
+
+    reply = if incorrect_reply?(config)
+      command.invalid_sample(config)
+    else
+      command.valid_sample
     end
+    return unless reply
+
+    PressDigits.new(ao_message, reply)
   end
 
   private def self.incorrect_reply?(config)
