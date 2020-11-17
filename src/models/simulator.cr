85f2428ff2cee759d6881c7bcb0891e382cfc6c8
aeatencio <aeatencio@gmail.com>
2020-11-17 12:49:04 -0300

    Honor the `delay replay` variables
    For #16

--- src/models/simulator.cr
+++ src/models/simulator.cr
@@ -15,14 +15,19 @@ module Twiliosim::Simulator
     end
     return unless reply
 
+    sleep delay_replay_seconds(config).seconds
     PressDigits.new(ao_message, reply)
   end
 
-  private def self.no_reply?(config)
+  private def self.delay_replay_seconds(config : Twiliosim::Config) : Int32
+    rand(config.delay_reply_min_seconds..config.delay_reply_max_seconds)
+  end
+
+  private def self.no_reply?(config : Twiliosim::Config) : Bool
     config.no_reply_percent >= rand
   end
 
-  private def self.incorrect_reply?(config)
+  private def self.incorrect_reply?(config : Twiliosim::Config) : Bool
     config.incorrect_reply_percent >= rand
   end
 
