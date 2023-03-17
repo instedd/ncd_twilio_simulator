63a01fd14cb7e2b2e7e2cbb8f8a6856dd45caaec
Julien Portalier <julien@portalier.com>
2023-03-17 16:14:59 +0100

    Fix: protect in memory database with a Mutex

--- src/db.cr
+++ src/db.cr
@@ -1,15 +1,20 @@
+require "json"
 require "./models/call"
+require "mutex"
 
 class Twiliosim::DB
   @calls = Hash(String, Twiliosim::Call).new
+  @mutex = Mutex.new
 
   def create_call(to : String, from : String, @account_sid : String) : Twiliosim::Call
     call = Twiliosim::Call.new(to, from, account_sid)
-    @calls[call.id] = call
+    @mutex.synchronize { @calls[call.id] = call }
   end
 
   def update_call(call : Twiliosim::Call) : Twiliosim::Call
-    raise "Cannot update an unexistent Call in DB - #{call.id}" unless @calls[call.id]
-    @calls[call.id] = call
+    @mutex.synchronize do
+      raise "Cannot update an unexistent Call in DB - #{call.id}" unless @calls[call.id]
+      @calls[call.id] = call
+    end
   end
 end
